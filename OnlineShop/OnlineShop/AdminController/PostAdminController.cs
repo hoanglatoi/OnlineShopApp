49ff3d1c16a06a363b8f679425d9671d0ce585c7
DESKTOP-PETP35B\hp <huynhducnguyen411@gmail.com>
2023-04-15 14:21:28 +0900

    Thêm Khung CRUD cho mục Post Content Admin___ (Chưa sửa chi tiết)

--- OnlineShop/OnlineShop/AdminController/PostAdminController.cs
+++ OnlineShop/OnlineShop/AdminController/PostAdminController.cs
@@ -1,12 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Model.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnlineShop.AdminController
 {
     public class PostAdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ShopOnlineDbContext _context;
+        public PostAdminController(ShopOnlineDbContext context) {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string searchString)
         {
+            var PostContentList = from m in _context.PostContents
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                PostContentList = PostContentList.Where(s => s.Name!.Contains(searchString));
+            }
+
+            return View(await PostContentList.ToListAsync());
+        }
+
+        public IActionResult Create() {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(string? imageFilePath)
+        {
+            // Get Lasted ID From DataBase
+            var PostContentOld = from m in _context.PostContents
+                                 select m;
+            
+            PostContent postContent = new PostContent();
+
+            postContent.Name = "Test1";
+            postContent.ID = 2;
+            postContent.Description = imageFilePath;
+
+            _context.Add(postContent);
+            await _context.SaveChangesAsync();
+
+            Console.WriteLine(imageFilePath);
+            Console.WriteLine("Test");
+
+            return View();
+        }
+        public async Task<IActionResult> ViewDetails(int? id)
+        {
+            if (id == null || _context.PostContents == null)
+            {
+                return NotFound();
+            }
+
+            var PostContent_Item = await _context.PostContents
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (PostContent_Item == null)
+            {
+                return NotFound();
+            }
+
+            return View(PostContent_Item);
         }
     }
 }
